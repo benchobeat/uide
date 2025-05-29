330df3947a3847e09b8e08e582dc69f01e84a563
benchobeat <ruben_mosquerav@hotmail.com>
2025-05-28 21:03:01 -0500

    Ejercicio 4

--- Clase1.py
+++ Clase1.py
@@ -2,8 +2,14 @@
 nombre = "Bienvenidos a la UIDE"    ## Variable de tipo cadena
 print(nombre)                       ## Imprime el mensaje en la consola
 """
-
+"""
 print("Ingrese su nombre: ")                                                            ## Solicita al usuario que ingrese su nombre
 nombre = input("Cual es tu nombre:")                                                    ## Captura el nombre ingresado por el usuario 
 edad = input("Cual es tu edad:")                                                        ## Captura la edad ingresada por el usuario
 print("Tu nombre es: " + nombre + ", tu edad es" + edad + ", bienvenido a la UIDE")     ## Imprime el nombre ingresado
+"""
+a = int(input(" Ingrese un numero: "))                  ## Solicita al usuario que ingrese un numero y lo convierte a entero
+b = int(input(" Ingrese otro numero: "))                 ## Solicita al usuario que ingrese otro numero y lo convierte a entero 
+suma = a + b                                        ## Suma los dos numeros ingresados
+print("La suma de los numeros es: " + str(suma)) 5   ## Imprime la suma de los numeros ingresados, convirtiendo el resultado a cadena  
+11
