f64651ea710831fed81ab5758b54773b6039c6c7
benchobeat <rmosquera@securesoftcorp.ec>
2025-05-28 20:41:33 -0500

    Ejercicio 2

--- Clase1.py
+++ Clase1.py
@@ -1,2 +1,8 @@
-nombre = "Bienvenidos a la UIDE"
-print(nombre)
+"""
+nombre = "Bienvenidos a la UIDE"    ## Variable de tipo cadena
+print(nombre)                       ## Imprime el mensaje en la consola
+"""
+
+print("Ingrese su nombre: ")                        ## Solicita al usuario que ingrese su nombre
+nombre = input()                                    ## Captura el nombre ingresado por el usuario 
+print("Hola " + nombre + ", bienvenido a la UIDE")  ## Imprime un saludo personalizado
