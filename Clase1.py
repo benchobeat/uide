5c53bab5274104f1c4d9384045040a88c2f49157
benchobeat <ruben_mosquerav@hotmail.com>
2025-05-28 20:54:07 -0500

    Ejercicio 3

--- Clase1.py
+++ Clase1.py
@@ -3,6 +3,7 @@ nombre = "Bienvenidos a la UIDE"    ## Variable de tipo cadena
 print(nombre)                       ## Imprime el mensaje en la consola
 """
 
-print("Ingrese su nombre: ")                        ## Solicita al usuario que ingrese su nombre
-nombre = input()                                    ## Captura el nombre ingresado por el usuario 
-print("Hola " + nombre + ", bienvenido a la UIDE")  ## Imprime un saludo personalizado
+print("Ingrese su nombre: ")                                                            ## Solicita al usuario que ingrese su nombre
+nombre = input("Cual es tu nombre:")                                                    ## Captura el nombre ingresado por el usuario 
+edad = input("Cual es tu edad:")                                                        ## Captura la edad ingresada por el usuario
+print("Tu nombre es: " + nombre + ", tu edad es" + edad + ", bienvenido a la UIDE")     ## Imprime el nombre ingresado
